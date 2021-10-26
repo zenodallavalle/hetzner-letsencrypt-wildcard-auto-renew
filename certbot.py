d4a2768014c400b101463fde4352796fbcb6310d
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-10-26 15:53:44 +0200

    fixed bugs, do not require certbot_path

--- certbot.py
+++ certbot.py
@@ -5,22 +5,35 @@ import hetzner
 import time
 import subprocess
 
+
 def get_acme_challenge(domain):
     return subprocess.check_output(["dig", "-t", "txt", f"_acme-challenge.{domain}", "+short"]).decode("utf-8").replace('"', "")
 
-def renew(zone, record, domain, cetbot_path):
+
+def renew(zone, record, domain):
     record = hetzner.get_acme_record(zone)
     old = record["value"]
-    child = pexpect.spawn(f"/bin/bash -c '{cetbot_path} certonly -d *.{domain} --server https://acme-v02.api.letsencrypt.org/directory --manual --preferred-challenges dns --manual-public-ip-logging-ok'", encoding="utf-8")
+    old = 'old'
+    child = pexpect.spawn(
+        f"/bin/bash -c 'certbot certonly -d *.{domain} --server https://acme-v02.api.letsencrypt.org/directory --manual --preferred-challenges dns --manual-public-ip-logging-ok'", encoding="utf-8")
     child.logfile_read = sys.stdout
 
-    ex = child.expect(["Before continuing", "Cert not yet due for renewal"])
+    ex = child.expect(
+        ["Before continuing", "Certificate not yet due for renewal"])
     if ex == 1:
-        print("Cert not due for renewal. Exiting.")
+        print("Certificate not yet due for renewal. Exiting.")
         sys.exit(0)
     elif ex == 0:
-        spl = child.before.split("\n")
-        new = spl[-3].strip()
+        i = 0
+        child.expect('\d*with the following value:\d*')
+        while i < 10:
+            new = child.readline().strip()
+            if new:
+                break
+        else:
+            print('Error while trying to read value, abort...')
+            sys.exit(0)
+
         if old != new:
             print(f"\n\n\nReplacing {old} with {new}")
 
--- renew.py
+++ renew.py
@@ -3,6 +3,7 @@ import certbot
 import sys
 import os
 
+
 def main():
     print("Let's Encrypt Wildcard Auto-Renewal with Hetzner\n")
 
@@ -14,18 +15,12 @@ def main():
     if (not "HETZNER_TOKEN" in os.environ):
         sys.exit("HETZNER_TOKEN environment variable is missing!")
 
-    if ("CERTBOT_PATH" in os.environ):
-        certbot_path = os.environ["CERTBOT_PATH"]
-    else:
-        certbot_path = "/opt/certbot/certbot-auto"
+    domain = sys.argv[1]
+    zone = hetzner.get_zone(domain)
+    record = hetzner.get_acme_record(zone)
+    certbot.renew(zone, record, domain)
+    certbot.renew()
 
-    if (os.path.exists(certbot_path)):
-        domain = sys.argv[1]
-        zone = hetzner.get_zone(domain)
-        record = hetzner.get_acme_record(zone)
-        certbot.renew(zone, record, domain, certbot_path)
-    else:
-        sys.exit("`certbot/certbot-auto` executable not found please set CERTBOT_PATH environment variable. Path `" + certbot_path + "` doesn't exist")
 
 if __name__ == "__main__":
     main()
